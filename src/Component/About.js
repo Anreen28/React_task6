89a0117ecb91b4012a048c43eb64a0119ac4418c
Anreen28 <anreenkaur22@gmail.com>
2023-11-03 22:06:48 +0530

    first commit

---
+++ src/Component/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="about">
+            <h2>About Me</h2>
+            <p>I am a third year student at VIT bhopal with keen interest in learning different technologies.</p>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section className="home">
+            <h1>ANREEN KOUR</h1>
+            <h1>Welcome to My Portfolio</h1>
+            <p> Data science and web development Enthusiast</p>
+        </section>
+    );
+}
+
+export default Home;
